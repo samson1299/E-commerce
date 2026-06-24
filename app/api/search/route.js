9d9f0e1d13f53f05887964e9821d4bfe49643c81
Saamson <sunkarasamsonsamson@gmail.com>
2026-06-24 21:45:18 +0530

    check  point

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -6,26 +6,12 @@ const client = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
 });
-async function genrateVector(text){
 
-}
 export async function POST(request) {
   try {
 
     const { query } = await request.json();
 
-    // const completion = await client.chat.completions.create({
-    //   model: "llama-3.3-70b-versatile",
-    //   messages: [
-    //     { role: "system", content: `Convert the user query into search keywords.Return comma separated keywords only. Example:"dumb bell" => dumbbell,weights,gym equipment,fitness"gaming keyboard" => keyboard,gaming,rgb,electronics` },
-    //     {
-    //       role: "user",
-    //       content: query,
-    //     },
-    //   ],
-    // });
-    // console.log(completion.choices[0].message.content);
-    // const keywords = completion.choices[0].message.content.split(",").map((k) => k.trim());
     const QueryEmbedding = embeddingRes.data[0].embedding; 
     await connectDB();
     const products = await Products.find({
